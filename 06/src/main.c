6a927629b381d063555673402bc86ba8be73666f
martistuwe <martistuwe@yahoo.de>
2018-06-12 21:16:00 +0200

    something

--- 06/src/main.c
+++ 06/src/main.c
@@ -1,9 +1,29 @@
-#include <pthread/pthread.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <dirent.h>
 #include "../include/miniz.h"
 #include "../include/queue.h"
 
+typedef struct Job {
+    const char *name;
+} Job;
+
+static Queue queue;
+
 int main(int argc, char const *argv[])
 {
-    /* code */
-    return 0;
+    if (argc != 2) {
+        printf("Falsche Anzahl Argumente\n");
+        return EXIT_FAILURE;
+    }
+
+    DIR *dir = opendir(argv[1]);
+    
+    if (dir != NULL){
+        queue = queue_create();
+    } else {
+        printf("Fehler beim öffnen des Verzeichnis\n");
+    }
+
+    return EXIT_SUCCESS;
 }
