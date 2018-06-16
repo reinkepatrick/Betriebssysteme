42224b53c3fde8de7a37be5954a27c7f712c6223
martistuwe <martistuwe@yahoo.de>
2018-06-16 22:08:04 +0200

    thread starting

--- 06/src/main.c
+++ 06/src/main.c
@@ -4,26 +4,75 @@
 #include "../include/miniz.h"
 #include "../include/queue.h"
 
-typedef struct Job {
-    const char *name;
+typedef struct Job
+{
+    const char *content;
+    const char *path;
 } Job;
 
-static Queue queue;
+typedef struct
+{
+    Queue queue;
+    pthread_mutex_t mutex;
+    int empty;
+} Jobs;
+
+typedef struct
+{
+    Jobs jobs;
+    const char *path;
+} Args;
+
+struct dirent *ent;
+
+void* reader_thread(void *arg)
+{
+    printf("Reader gestartet.\n");
+
+    /*
+    Args *reader_arg = (Args *) arg;
+
+    DIR *dir = opendir(reader_arg->path);
+    if (dir != NULL)
+    {
+        while ((ent = readdir(dir)) != NULL)
+            queue_insert(NULL, NULL);
+    }
+    else
+    {
+        printf("Fehler beim öffnen des Verzeichnis\n");
+    }
+    */
+    return (void *)NULL;
+}
 
 int main(int argc, char const *argv[])
 {
-    if (argc != 2) {
+    if (argc != 2)
+    {
         printf("Falsche Anzahl Argumente\n");
         return EXIT_FAILURE;
     }
 
-    DIR *dir = opendir(argv[1]);
-    
-    if (dir != NULL){
-        queue = queue_create();
-    } else {
-        printf("Fehler beim öffnen des Verzeichnis\n");
+    //Init Queue
+    Jobs jobs;
+    jobs.queue = queue_create();
+    pthread_mutex_init(&jobs.mutex, NULL);
+
+    //Arguments for Threads
+    Args args;
+    args.jobs = jobs;
+    args.path = argv[1];
+
+    //
+    pthread_t reader, compress;
+    if(pthread_create(&reader, NULL, &reader_thread, &args) != 0) 
+    {
+        printf("Fehler beim Erstellen des Readers\n");
+        return EXIT_FAILURE;
     }
+
+    pthread_join(reader, NULL);
 
     return EXIT_SUCCESS;
 }
